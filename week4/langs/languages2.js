04091f61e37ed07a9ccca0ba8c36812986c67fff
yesi2023 <irma.y.rodriguez@bryanuniversity.edu>
2022-04-20 22:37:33 -0500

    lang

--- week4/langs/languages2.js
+++ week4/langs/languages2.js
@@ -1,19 +1,23 @@
-const lang = 'JavaScript'
+const lang = "JavaScript";
 
-// Create an event listener for the submit button that adds all 'input' elements to 
-// a single array using the spread operator. Call the chkLang() function, passing in 
+// Create an event listener for the submit button that adds all 'input' elements to
+// a single array using the spread operator. Call the chkLang() function, passing in
 // the array as an argument.
 
+document.getElementById("submit").addEventListener("click", function () {
+  let allElements = document.querySelectorAll("input");
+  let arrayOfAllElements = [...allElements];
+
+  chkLang(arrayOfAllElements);
+});
 
 function chkLang(langs) {
-    let result = false
+  let result = false;
 
-    // use an array method to check whether the user included 'JavaScript' in their
-    // list of languages
+  // use an array method to check whether the user included 'JavaScript' in their
+  // list of languages
 
-    let obj = document.querySelector('#TestResult')
-    if (result) 
-        obj.innerText = `Congratulations!\nYou know ${lang}.`
-    else
-        obj.innerText = `Sorry,\nyou don't know ${lang}.`
+  let obj = document.querySelector("#TestResult");
+  if (result) obj.innerText = `Congratulations!\nYou know ${lang}.`;
+  else obj.innerText = `Sorry,\nyou don't know ${lang}.`;
 }
